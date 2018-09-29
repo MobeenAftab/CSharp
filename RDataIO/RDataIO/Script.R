ea9eb04c757153baef6df1d0d6218b0633b0c993
mobeenaftab <mobeenaftab1@gmail.com>
2018-09-29 22:39:23 +0100

    RDatIO
    Testing fer2018 results in Java Heap error

--- RDataIO/RDataIO/Script.R
+++ RDataIO/RDataIO/Script.R
@@ -3,12 +3,16 @@ library("RWeka")
 library("RWekajars")
 #search()
 
+#options(java.parameters = "-Xmx2048m")
+options(java.parameters = "-Xmx8000m")
+
+library(rJava)
+
 #installed.packages()
 
 # Read soybean dataset from arff file
-data1 = read.csv(file.choose(), header = FALSE, sep = ",")
-
-data1
+# data1 = read.csv(file.choose(), header = FALSE, sep = ",")
+# data1
 # Read edited soybean file, csv
 
 # Write arff file
@@ -16,4 +20,5 @@ data1
 # > soybean <- read.csv(file="C:/Users/Mobeen/AppData/Local/Temp/soybean.csv.utf8", header=FALSE, row.names=NULL, encoding="UTF-8", sep=",", dec=".", quote="\"", comment.char="")
 
 
-
+fer2018 <- read.csv(file.choose(), header = FALSE, row.names = NULL, encoding = "UTF-8", sep = ",", dec = ".", quote = "\"", comment.char = "")
+write.arff(fer2018, file = "fer2018.arff", eol = "\n")
